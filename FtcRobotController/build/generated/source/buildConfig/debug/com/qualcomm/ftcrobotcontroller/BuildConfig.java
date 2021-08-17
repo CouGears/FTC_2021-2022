43cacc0587f249e69f3ff5a99bbe5303096e1315
Aaronthetechguy <aaron.goldgewert@gmail.com>
2021-08-17 17:12:52 -0400

    tested webcam init within class

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String BUILD_TIME = "2021-08-11T18:04:24.907-0400";
+  public static final String BUILD_TIME = "2021-08-17T17:11:36.341-0400";
 }
