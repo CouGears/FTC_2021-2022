631b34201ce585453f24efd3a21e0110585b5757
Aaronthetechguy <aaron.goldgewert@gmail.com>
2021-09-20 14:32:59 -0400

    updated test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode.java
@@ -42,7 +42,7 @@ public class TestCode extends LinearOpMode{
                 motorFR.setPower(gamepad1.right_trigger);
 
             } else if (gamepad1.left_trigger > 0){
-                motorFR.setPower(gamepad1.left_trigger);
+                motorFR.setPower(-gamepad1.left_trigger);
             } else{
                 motorFR.setPower(0);
             }
