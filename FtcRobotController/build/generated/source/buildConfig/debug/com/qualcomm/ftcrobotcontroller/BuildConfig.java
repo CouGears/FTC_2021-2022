83cfcaeefff65098b0f88e1b2d51840ed451150a
Aaronthetechguy <aaron.goldgewert@gmail.com>
2022-01-26 16:48:08 -0500

    rebased code on ftc robot controller version 7.1

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -7,8 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 42;
-  public static final String VERSION_NAME = "7.0";
-  // Fields from default config.
-  public static final String APP_BUILD_TIME = "2022-01-24T20:48:48.231-0500";
+  // Field from default config.
+  public static final String APP_BUILD_TIME = "2022-01-26T16:47:32.602-0500";
 }
