82656e4b8ec4bf6f0aaf4815c5b3242c869cf83c
danielsarasohn <59397382+danielsarasohn@users.noreply.github.com>
2022-02-15 16:31:49 -0500

    basic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionDriving2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionDriving2021.java
@@ -12,8 +12,8 @@ import com.qualcomm.robotcore.hardware.CRServo;
 public class CompetitionDriving2021 extends LinearOpMode {
 
     private DcMotor motorBR, motorBL, motorFL, motorFR, intake, lifter, carousel, lift;
-    private Servo bucket, intakeServo, liftyThingy, hServo, vServo;
-    private CRServo dServo;
+    private Servo bucket, intakeServo, liftyThingy;//, hServo, vServo;
+    //private CRServo dServo;
     private boolean claw = false, bucketButton = false;
     private AutonMethods robot = new AutonMethods();
     int x = 0;
@@ -42,9 +42,9 @@ public class CompetitionDriving2021 extends LinearOpMode {
         intakeServo = hardwareMap.get(Servo.class, "serv");
         liftyThingy = hardwareMap.get(Servo.class, "liftyThingy");
 
-        hServo = hardwareMap.get(Servo.class, "hServo");
-        vServo = hardwareMap.get(Servo.class, "vServo");
-        dServo = hardwareMap.crservo.get("dServo");
+       // hServo = hardwareMap.get(Servo.class, "hServo");
+       // vServo = hardwareMap.get(Servo.class, "vServo");
+      //  dServo = hardwareMap.crservo.get("dServo");
 
 
 
@@ -54,7 +54,6 @@ public class CompetitionDriving2021 extends LinearOpMode {
         motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         carousel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -117,28 +116,15 @@ public class CompetitionDriving2021 extends LinearOpMode {
                 intake.setPower(0);
             }
 
-//hello there
-            if (gamepad1.dpad_up) {
-                lifter.setPower(-.6);
+            if (gamepad1.dpad_up)lifter.setPower(-.8);
+            else if (gamepad1.dpad_down) lifter.setPower(.8);
+            else lifter.setPower(0);
 
-            } else if (gamepad1.dpad_down) {
-                lifter.setPower(.6);
 
-            } else {
-                lifter.setPower(0);
+            if (gamepad1.y)lift.setPower(.6);
+            else if (gamepad1.a)lift.setPower(-.6);
+            else lift.setPower(0);
 
-            }
-            if (gamepad1.y) {
-
-                lift.setPower(.6);
-
-            } else if (gamepad1.a) {
-                lift.setPower(-.6);
-
-            } else {
-                lift.setPower(0);
-
-            }
             if (gamepad1.x) {
                 if (SWITCH == 0) {
                     robot.newSleep(.5);
@@ -147,7 +133,6 @@ public class CompetitionDriving2021 extends LinearOpMode {
                     robot.newSleep(.5);
                     SWITCH--;
                 }
-
             }
             if (SWITCH==0)
             {
@@ -157,32 +142,20 @@ public class CompetitionDriving2021 extends LinearOpMode {
             {
                 liftyThingy.setPosition(.66+(gamepad1.right_trigger*.33));
             }
-          /*  */
 
-            if (gamepad1.dpad_left) {
-                carousel.setPower(.7);
-                //robot.sleep(2000);
-                //carousel.setPower(0);
-            } else if (gamepad1.dpad_right) carousel.setPower(-.7);
+            if (gamepad1.dpad_left) carousel.setPower(.7);
+            else if (gamepad1.dpad_right) carousel.setPower(-.7);
             else carousel.setPower(0);//set ===to while else??
             
             if (xtape <= .97 && xtape >= -.97) xtape = xtape + this.gamepad2.right_stick_x * .03;
             if (ytape <= .97 && ytape >= -.97) ytape = ytape + this.gamepad2.right_stick_y * .03;
-            if (gamepad2.right_bumper){
-                extendpower = pextend;
-            } else if (gamepad2.right_trigger) {
-                extendpower = -pextend;
-            }
-            else {
-                extendpower = 0;
-            }
 
-            hServo.setPosition(ytape);
-            vServo.setPosition(xtape);
-            dServo.setPower(extendpower);
-            
-
-
+            if (gamepad2.right_bumper)extendpower = pextend;
+            else if (gamepad2.right_bumper)extendpower = -pextend;
+            else extendpower = 0;
+            //hServo.setPosition(ytape);
+            //vServo.setPosition(xtape);
+            //dServo.setPower(extendpower);
         }
     }
 }
